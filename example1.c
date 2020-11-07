8fbae7cb36fe322873aaed07cd92aed67e1ef932
Matthias Güdemann <matthias.guedemann@hm.edu>
2020-11-07 11:20:37 +0100

    Frama-C proof abs

--- example1.c
+++ example1.c
@@ -2,6 +2,18 @@
    zu zeigen: abs berechnet den Betrag
  */
 
+/*@
+  behavior negative:
+    assumes x < 0;
+    ensures \result == -x;
+
+  behavior non_negative:
+    assumes x >= 0;
+    ensures \result == x;
+
+  complete behaviors;
+  disjoint behaviors;
+ */
 int abs (int x) {
   int r;
   if (x >= 0) {
